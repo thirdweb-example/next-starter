c6003a47688c78d034ad5f80f55c856c5695b171
Manan Tank <manantankm@gmail.com>
2024-03-07 02:39:57 +0530

    mediarenderer test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,84 @@
-import Image from 'next/image';
-import { ConnectButton } from '@/app/thirdweb';
-import thirdwebIcon from '@public/thirdweb.svg';
+import { MediaRenderer } from '@/app/thirdweb';
+
+const medias = {
+	videos: [
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/20.webm',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/0.3gp',
+		'ipfs://QmZqLEvLjvHRQ8z62PUBkdsAHtHFpXxgAh2aViYqL5ntRb/29.m4v',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/10.mp4',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/8.mkv',
+		'ipfs://Qmd8dN3cfEqCkPPUHJ2Bhd8EKVmBectfZhMEjDiyAcB9kV/30.mov',
+		// 'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/13.ogv',
+	],
+	images: [
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/21.webp',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/6.jpeg',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/16.svg',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/18.tiff',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/7.jpg',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/3.gif',
+	],
+	audio: [
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/1.m4a',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/2.flac',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/9.mp3',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/11.oga',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/12.ogg',
+		'ipfs://Qmf2Yg8CNG9mT5UF1cSFAhZxxEenNJrgj5mFGJufbZQe3r/28.wav',
+	],
+	model: ['ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/4.glb'],
+	html: ['ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/5.html'],
+	others: [
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/14.pdf',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/17.txt',
+		'ipfs://QmfYtQW1Pr6dqx9iyK4B4cPjpt43PnkZmy858MMcBDssoF/19.usdz',
+	],
+};
 
 export default function Home() {
 	return (
 		<main className='p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto'>
 			<div className='py-20'>
-				<Header />
-
-				<div className='flex justify-center mb-20'>
-					<ConnectButton />
+				<h1 className='text-8xl mb-12 font-semibold tracking-tighter'> MediaRenderer </h1>
+				<div className='flex flex-col gap-20'>
+					<Category title='Videos' medias={medias.videos} />
+					<Category title='Images' medias={medias.images} />
+					<Category title='Audio' medias={medias.audio} />
+					<Category title='HTML' medias={medias.html} />
+					<Category title='Others' medias={medias.others} />
 				</div>
-
-				<ThirdwebResources />
 			</div>
 		</main>
 	);
 }
 
-function Header() {
+function Category(props: { title: string; medias: string[] }) {
 	return (
-		<header className='flex flex-col items-center mb-20 md:mb-20'>
-			<Image
-				src={thirdwebIcon}
-				alt=''
-				className='size-[150px] md:size-[150px]'
-				style={{
-					filter: 'drop-shadow(0px 0px 24px #a726a9a8)',
-				}}
-			/>
-
-			<h1 className='text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100'>
-				thirdweb unified SDK
-				<span className='text-zinc-300 inline-block mx-1'> + </span>
-				<span className='inline-block -skew-x-6 text-blue-500'> Next.js </span>
-			</h1>
-
-			<p className='text-zinc-300 text-base'>
-				Read the{' '}
-				<code className='bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1'>README.md</code>{' '}
-				file to get started.
-			</p>
-		</header>
-	);
-}
-
-function ThirdwebResources() {
-	return (
-		<div className='grid gap-4 lg:grid-cols-3 justify-center'>
-			<ArticleCard
-				title='thirdweb unified SDK Docs'
-				href='https://portal.thirdweb.com/typescript/v5'
-				description='thirdweb TypeScript Unified SDK documentation'
-			/>
-
-			<ArticleCard
-				title='Components and Hooks'
-				href='https://portal.thirdweb.com/typescript/v5/react'
-				description='Learn about the thirdweb React components and hooks in thirdweb unified SDK'
-			/>
-
-			<ArticleCard
-				title='thirdweb Dashboard'
-				href='https://thirdweb.com/dashboard'
-				description='Deploy, configure, and manage your smart contracts from the dashboard.'
-			/>
+		<div>
+			<h2 className='text-7xl my-10 font-semibold tracking-tighter'> {props.title} </h2>
+			<div className='grid md:grid-cols-2 xl:grid-cols-3 gap-6'>
+				{props.medias.map(mediaUrl => {
+					const split = mediaUrl.split('.');
+					const extension = split[split.length - 1];
+					return (
+						<div
+							key={mediaUrl}
+							className='border border-zinc-800 p-4 rounded-lg'
+							style={{
+								background: `linear-gradient(120deg, rgba(255, 255, 255, 0.03), rgba(0, 0, 0, 0.01))`,
+							}}
+						>
+							<p className='mb-6 font-medium text-2xl text-zinc-400'>{extension}</p>
+							<MediaRenderer
+								src={mediaUrl}
+								className='!rounded-lg !overflow-hidden'
+								width='250px'
+								height='250px'
+							/>
+						</div>
+					);
+				})}
+			</div>
 		</div>
-	);
-}
-
-function ArticleCard(props: { title: string; href: string; description: string }) {
-	return (
-		<a
-			href={props.href + '?utm_source=next-template'}
-			target='_blank'
-			className='flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700'
-		>
-			<article>
-				<h2 className='text-lg font-semibold mb-2'>{props.title}</h2>
-				<p className='text-sm text-zinc-400'>{props.description}</p>
-			</article>
-		</a>
 	);
 }
