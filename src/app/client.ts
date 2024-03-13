fbfa5e67ea81672a718c24136ef8759b1410bc34
Manan Tank <manantankm@gmail.com>
2024-03-14 04:21:23 +0530

    hardcode client id

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,13 +1,5 @@
 import { createThirdwebClient } from 'thirdweb';
 
-// Replace this with your client ID string
-// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
-
-if (!clientId) {
-	throw new Error('No client ID provided');
-}
-
 export const client = createThirdwebClient({
-	clientId: clientId,
+	clientId: '92bb0cd9e9055de30502c1501dc8c2fe',
 });
