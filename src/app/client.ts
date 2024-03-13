84b9063fda15a770f01141beddd4b0146f80f095
Manan Tank <manantankm@gmail.com>
2024-03-14 04:40:05 +0530

    configurable client id

--- src/app/client.ts
+++
@@ -1,5 +0,0 @@
-import { createThirdwebClient } from 'thirdweb';
-
-export const client = createThirdwebClient({
-	clientId: '92bb0cd9e9055de30502c1501dc8c2fe',
-});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,41 @@
-import type { Metadata } from 'next';
+'use client';
+
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThirdwebProvider } from '@/app/thirdweb';
-import { client } from '@/app/client';
+import { createThirdwebClient } from 'thirdweb';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-	title: 'thirdweb SDK + Next starter',
-	description: 'Starter template for using thirdweb SDK with Next.js App router',
-};
 
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const [clientId, setClientId] = useState('92bb0cd9e9055de30502c1501dc8c2fe');
+	const client = createThirdwebClient({
+		clientId: clientId,
+	});
+
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
+				<div className='flex items-center p-4 flex-col gap-3 max-w-screen-lg m-auto pt-10'>
+					<label htmlFor='client-id' className='text-zinc-300'>
+						{' '}
+						Client ID{' '}
+					</label>
+					<input
+						id='client-id'
+						type='text'
+						value={clientId}
+						onChange={e => {
+							setClientId(e.target.value);
+						}}
+						className='bg-zinc-900 border text-sm p-3 rounded-lg w-[400px] border-zinc-800 text-zinc-100 text-center'
+					/>
+				</div>
 				<ThirdwebProvider client={client}>{children}</ThirdwebProvider>
 			</body>
 		</html>
