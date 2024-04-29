55413929723730a4d0e0404186847a57ace96a9d
Joaquim Verges <joaquim.verges@gmail.com>
2024-04-29 12:43:00 +1200

    Fix WC dependency warning with SSR

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
 
 export default nextConfig;
