5d05169e61c1e6933c7ace5ee903dd3a97cbd0f6
Manan Tank <manantankm@gmail.com>
2024-06-24 20:56:43 +0530

    test-1399

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,98 +1,84 @@
+"use client";
+
 import Image from "next/image";
 import { ConnectButton } from "thirdweb/react";
 import thirdwebIcon from "@public/thirdweb.svg";
 import { client } from "./client";
+import { createWallet, walletConnect, inAppWallet } from "thirdweb/wallets";
+import { polygon } from "thirdweb/chains";
+
+const wallets = [
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  walletConnect(),
+  inAppWallet({
+    auth: {
+      options: ["email", "google", "apple", "facebook", "phone"],
+    },
+    smartAccount: {
+      chain: polygon,
+      sponsorGas: true,
+    },
+  }),
+  createWallet("com.trustwallet.app"),
+  createWallet("io.zerion.wallet"),
+  createWallet("app.phantom"),
+];
 
 export default function Home() {
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        <Header />
-
+      <div className="">
         <div className="flex justify-center mb-20">
           <ConnectButton
+            wallets={wallets}
             client={client}
             appMetadata={{
               name: "Example App",
               url: "https://example.com",
             }}
+            theme={"dark"}
+            connectButton={{ label: "Login" }}
+            detailsButton={{
+              displayBalanceToken: {
+                [polygon.id]: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+              },
+            }}
+            connectModal={{
+              size: "wide",
+              title: "UltimateDeal",
+              titleIcon: "https://ultimateDeal.net/images/logoOfWebsite.svg",
+              welcomeScreen: {
+                title: "UltimateDeal",
+                subtitle:
+                  "Make your first step to the journey of your life. Contribute to businesses anonymously and get shares in return and dividends. Open a Crowdfunding campaign and issue your business shares to the public. Get started by connecting your wallet.",
+                img: {
+                  src: "https://ultimateDeal.net/images/logoOfWebsite.svg",
+                  width: 150,
+                  height: 150,
+                },
+              },
+              termsOfServiceUrl: "https://ultimateDeal.net/terms",
+              privacyPolicyUrl: "https://ultimateDeal.net/privacy-policy",
+              showThirdwebBranding: false,
+            }}
+            supportedTokens={{
+              [polygon.id]: [
+                {
+                  address: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+                  name: "USDT",
+                  symbol: "USDT",
+                },
+              ],
+            }}
+            chain={polygon}
+            switchButton={{
+              label: "Switch Network",
+              className: "my-custom-class",
+            }}
           />
         </div>
-
-        <ThirdwebResources />
       </div>
     </main>
-  );
-}
-
-function Header() {
-  return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
-      <Image
-        src={thirdwebIcon}
-        alt=""
-        className="size-[150px] md:size-[150px]"
-        style={{
-          filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-        }}
-      />
-
-      <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        thirdweb SDK
-        <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-blue-500"> Next.js </span>
-      </h1>
-
-      <p className="text-zinc-300 text-base">
-        Read the{" "}
-        <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-          README.md
-        </code>{" "}
-        file to get started.
-      </p>
-    </header>
-  );
-}
-
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="thirdweb SDK Docs"
-        href="https://portal.thirdweb.com/typescript/v5"
-        description="thirdweb TypeScript SDK documentation"
-      />
-
-      <ArticleCard
-        title="Components and Hooks"
-        href="https://portal.thirdweb.com/typescript/v5/react"
-        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-      />
-
-      <ArticleCard
-        title="thirdweb Dashboard"
-        href="https://thirdweb.com/dashboard"
-        description="Deploy, configure, and manage your smart contracts from the dashboard."
-      />
-    </div>
-  );
-}
-
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
-  return (
-    <a
-      href={props.href + "?utm_source=next-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
   );
 }
