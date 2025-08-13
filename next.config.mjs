67434b5c4fbd88dee744e9abdc9f52c55bccc9d7
Manan Tank <manantankm@gmail.com>
2025-08-13 21:44:55 +0530

    Update to Next 15

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
+  serverExternalPackages: ["pino-pretty"],
 };
 
 export default nextConfig;
