6773f3cebd0da90545cb16772ec7669ff28c1244
ZCarp <87540457+ZCarp@users.noreply.github.com>
2025-09-11 16:54:45 +0000

    Attempt redeploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,28 +24,8 @@ const TOKENS = [
 
 export default function Home() {
   return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        <Header />
-
-        <div className="flex flex-col items-center mb-20">
-          <ConnectButton
-            client={client}
-            appMetadata={{
-              name: "Example App",
-              url: "https://example.com",
-            }}
-          />
-          <PlatinumMembership />
-          <CyberfamMembership />
-          <MintFromDrop />
-          <ListNFTForSale />
-          <MarketplaceListings />
-          <MyNFTs />
-        </div>
-
-        <ThirdwebResources />
-      </div>
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello, world!</h1>
     </main>
   );
 }
